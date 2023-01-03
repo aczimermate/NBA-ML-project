ed8dc63127dde3bea0b4da7b13dba00a3bca5086
aczimermate <czimer21@gmail.com>
2023-01-03 12:48:52 +0100

    modified:   EDA.ipynb 	modified:   __pycache__/evaluation_framework.cpython-39.pyc 	new file:   corr_matrix_small.png 	modified:   evaluation_framework.py

--- evaluation_framework.py
+++ evaluation_framework.py
@@ -1,80 +1,47 @@
 # necessary libraries for the evaluation framework
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.metrics import confusion_matrix, classification_report
 
-class LogRegModel:
+class evaluation_framework:
     '''
-    Creating a logistic regression class instance indicates a train/test split of the data 
-    and training the classification model on the training set followed by a prediction on the test values.
+    This class contains all attributes and necessary procedures for classification model construction and evaluation. 
+    The training of classification model on the training set followed by the prediction of the y vector based on the test feature space.
 
-    The class uses methods from the sklearn libraries below: 
+    The class uses methods from the scikit-learn libraries listed below: 
      - sklearn.linear_model.LogisticRegression
      - sklearn.model_selection
      - sklearn.metrics.confusion_matrix
      - sklearn.metrics.classification_report
 
-
-    Parameters:
-     - Data set containing the X feature space predictors and the y answer vector. 
-       The train/test split takes place during the evaluation process.
+    Input parameters:
+     - Data set containing the X feature space predictors and the y answer vector, and the desired test size for the train/test split. 
        
     Methods:
-     - X_train:
-     - X_test:
-     - y_train:
-     - y_test:
-     - model:
-     - test_results:
+     - split_data: train/test split of the data set
+     - classify: construction method of the classification model
+     - evaluate: evaluation of the test results 
     '''
 
-    def __init__(self, X, y, test_size):
-        
-        # split the data into training a test sets
+    def split_data(X, y, test_size):
+        '''
+        Split the data into training a test sets, then return the splitted data set.
+        '''
         X_train, y_train, X_test, y_test = train_test_split(X,y,test_size=test_size)
+        return X_train, y_train, X_test, y_test
 
-        # create instance features for later usage
-        self.X_train = np.to_array(X_train)
-        self.X_test = np.to_array(X_test)
-        self.y_train = np.to_array(y_train)
-        self.y_test = np.to_array(y_test)
-
-        # fit the logistic regression model to the training data
-        self.model = LR().fit(X_train,y_train)
-        self.prediction = self.model.predict(X_test)
-        self.prediction_results = self.prediction.score()
-
-
-    def X_train(self):
-        return (self.X_train)
-
-    def X_test(self):
-            return (self.X_test)
-
-    def y_train(self):
-            return (self.y_train)
-
-    def y_test(self):
-            return (self.y_test)
-
-    def model_params(self):
+    def classify(X_train, y_train, X_test, y_test):
         '''
-        Return the training model parameters. 
+        Fit the classification model to the training data.
         '''
-        return self.model.get_params()
+        model = LR().fit(X_train,y_train)
+        y_pred = model.predict(X_test)
+        return y_pred
 
-    def results(self):
+    def evaluate(y_pred, y_test):
         '''
-        Return the test model evaluation results. 
+        Return the evaluation results of the classification model. 
         '''
-        score = self.prediction_results
-        confusion_matrix
-        classification_report
-        return score
-
-    class ClassEvaluator:
-        '''
-        '''
-        def __init__(self,model):
-             self.model = model
+        cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
+        cp = classification_report(y_pred=y_pred, y_true=y_test)
+        return cm,cp
